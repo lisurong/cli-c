4ccbd5c9fef31934ea83d01410f16f5294730d18
lisurong <lisurong@300.cn>
2019-02-19 11:01:22 +0800

    11

---
+++ bin/template/jsTemp.js
@@ -0,0 +1,28 @@
+$define(["wgt_util", "wgt_readyload"], function ($, e, r) {
+    /**
+     *定义公有 初始化方法
+    */
+  
+   
+    
+
+
+    
+    var init = function () {
+        /**
+         * 调用定义初始化函数
+         */
+        //获取页面参数
+        params = this._params;
+
+                
+        
+    };
+
+
+
+    return {
+        
+        init: init
+    };
+})
--- project.js
+++ project.js
@@ -5,27 +5,33 @@ module.exports = {
     }, {
         type: 'file',
         name: 'c_{{name}}-01001.css',
-        source:'template/index.css',
+        source:'template/cssTemp.css',
     }, {
         type: 'file',
         name: 'c_{{name}}-01001.html',
+        source:'template/htmlTempA.html',
     },{
         type: 'file',
         name: 'c_{{name}}-01001.properties',
+        source:'template/propTemp.properties',
     },{
         type: 'file',
         name: '{{name}}-01.js',
+        source:'template/jsTemp.js',
     },{
         type: 'file',
-        name: '{{name}}-01.min.js',
+        name: '{{name}}-01.min.js'
     },{
         type: 'file',
         name: 'c_{{name}}-01.json',
+        source:'template/jsonTemp.json',
     }, {
         type: 'file',
         name: '{{name}}-01.xml',
+        source:'template/xmlTemp.xml',
     }, {
         type: 'file',
         name: '{{name}}-01.html',
+        source:'template/htmlTempB.html',
     }]
 }

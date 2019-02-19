16cba43767a7359c57c9cae93350bc7b46873dd7
lisurong <lisurong@300.cn>
2019-02-19 10:19:50 +0800

    tijiao

--- project.js
+++ project.js
@@ -4,10 +4,28 @@ module.exports = {
         name: 'js'
     }, {
         type: 'file',
-        name: 'ccc_{{name}}_001.css',
+        name: 'c_{{name}}-01001.css',
         source:'template/index.css',
     }, {
         type: 'file',
-        name: 'cccc_{{name}}_001.js',
+        name: 'c_{{name}}-01001.html',
+    },{
+        type: 'file',
+        name: 'c_{{name}}-01001.properties',
+    },{
+        type: 'file',
+        name: '{{name}}-01.js',
+    },{
+        type: 'file',
+        name: '{{name}}-01.min.js',
+    },{
+        type: 'file',
+        name: 'c_{{name}}-01.json',
+    }, {
+        type: 'file',
+        name: '{{name}}-01.xml',
+    }, {
+        type: 'file',
+        name: '{{name}}-01.html',
     }]
 }
